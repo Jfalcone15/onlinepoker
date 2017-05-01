4706688fad7a69798e69b0690a4bb8edc9af8a95
Jfalcone15 <jfalcone15@att.net>
2017-05-01 18:19:39 -0400

    Prototype
    Beta Prototype

---
+++ BetaPrototype/BetaPrototype/BetaPrototype/MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BetaPrototype
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MakeTransaction finance = new MakeTransaction();
+            finance.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GameCreation newgame = new GameCreation();
+            newgame.Show(); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            LoadingGames newsearch = new LoadingGames();
+            newsearch.Show();
+        }
+    }
+}
---
+++ BetaPrototype/BetaPrototype/BetaPrototype/WithdrawWinnings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BetaPrototype
+{
+    public partial class WithdrawWinnings : Form
+    {
+        public WithdrawWinnings()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Transaction successful!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
